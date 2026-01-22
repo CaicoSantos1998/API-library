2a633dc9910d61e1f39ca7a62ced6966f0f9b411
CS <caico_fc@hotmail.com>
2026-01-22 14:26:46 -0300

    Project start

---
+++ src/main/java/github/caicosantos/library/api/application/Application/Program.java
@@ -0,0 +1,13 @@
+package github.caicosantos.library.api.application.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program.class, args);
+	}
+
+}
---
+++ src/test/java/github/caicosantos/library/api/ProgramTests.java
@@ -0,0 +1,13 @@
+package github.caicosantos.library.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
