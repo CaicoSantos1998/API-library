cf6ccc126cf13fbed97e0a4c9d4f7986b4348685
CS <caico_fc@hotmail.com>
2026-01-22 14:39:13 -0300

    Connection to the  database established

---
+++ src/main/java/github/caicosantos/library/api/config/DatabaseConfig.java
@@ -0,0 +1,40 @@
+package github.caicosantos.library.api.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${pring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource hikariDatabaseSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setPoolName("lib_db-pool");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setMaxLifetime(300000);
+        config.setConnectionTimeout(45000);
+        config.setConnectionTestQuery("Select 1");
+
+        return new HikariDataSource(config);
+    }
+}
