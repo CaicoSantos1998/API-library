2210a09874e04bb92914e925d05a787873be2357
CS <caico_fc@hotmail.com>
2026-01-30 22:12:24 -0300

    The method for saving the book was implemented, and a generic auxiliary controller class was created

--- src/main/java/github/caicosantos/library/api/controller/BookController.java
+++ src/main/java/github/caicosantos/library/api/controller/BookController.java
@@ -1,11 +1,39 @@
 package github.caicosantos.library.api.controller;
 
+import github.caicosantos.library.api.controller.dto.BookRegistrationDTO;
+import github.caicosantos.library.api.controller.dto.ErrorResponse;
+import github.caicosantos.library.api.controller.mappers.BookMapper;
+import github.caicosantos.library.api.exceptions.DuplicateRegisterException;
+import github.caicosantos.library.api.model.Book;
 import github.caicosantos.library.api.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class BookController {
+@RequestMapping("/books")
+public class BookController implements GenericController {
     private final BookService bookService;
+    private final BookMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid BookRegistrationDTO dto) {
+        try {
+            Book book = mapper.toEntity(dto);
+            bookService.save(book);
+            return ResponseEntity
+                    .created(generateHeaderLocation(book.getId()))
+                    .build();
+        } catch (DuplicateRegisterException e) {
+            var errorDTO = ErrorResponse.conflict(e.getMessage());
+            return ResponseEntity
+                    .status(errorDTO.status())
+                    .body(errorDTO);
+        }
+    }
 }
--- src/main/java/github/caicosantos/library/api/service/BookService.java
+++ src/main/java/github/caicosantos/library/api/service/BookService.java
@@ -1,5 +1,6 @@
 package github.caicosantos.library.api.service;
 
+import github.caicosantos.library.api.model.Book;
 import github.caicosantos.library.api.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Service;
 public class BookService {
     private final BookRepository bookRepository;
 
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
 }
